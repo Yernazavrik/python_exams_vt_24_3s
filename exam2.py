cf7e256af90142d203f1e0b06136d1c0407459b7
Yernazavrik <ernazarkanafin2004@yandex.kz>
2025-12-08 15:02:20 +0500

    retake_1

--- exam2.py
+++ exam2.py
@@ -1,11 +1,11 @@
 #2 1 вариант
-s = "abcde" 
+s = "aeees" 
 def count_vowels(s, i = 0, sum = 0):
-	if s == "":
+	if i == len(s):
 		return sum
 	vowels = "aeoiu"
-	print(s[i])
 	if s[i] in vowels:
-		s[i] =""
 		return count_vowels(s, i+1, sum+1)
+	else:
+		return count_vowels(s, i+1, sum)
 print(count_vowels(s))
--- fabric.py
+++ fabric.py
@@ -2,11 +2,11 @@
 from PyQt5 import QtWidgets, QtGui, QtCore
 import PyQt5
 import sys
-
 class BaseButton(QtWidgets.QPushButton):
     def __init__(self, text):
         super().__init__(text)
-        
+    def create_button(self, button_type:str) -> QtWidgets.QPushButton:
+        pass
 class DangerButton(BaseButton):
     def __init__(self):
         super().__init__("danger button")
@@ -28,7 +28,7 @@ class Selector():
         if button_type == "success":
             return SuccessButton()
         if button_type == "default":
-            return DefaultButton
+            return DefaultButton()
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -41,7 +41,13 @@ class MainWindow(QtWidgets.QMainWindow):
         self.button = QtWidgets.QPushButton("Нажми для выбора кнопки")
         self.layout.addWidget(self.line_edit)
         self.button.clicked.connect(self.onClick)
-        
+        self.layout.addWidget(self.button)
+        self.setCentralWidget(self.widget)
+    def onClick(self):
+            self.factory = Selector.choose_button(self.line_edit.text())
+            self.buttonType = self.factory.create_button("")
+            self.layout.addWidget(self.buttonType)
+            
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
